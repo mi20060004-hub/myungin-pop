2cb2be4d468f55c05ef791d2b869ae6059a131c9
mi20060004-hub <mi20060004@gmail.com>
2026-06-17 10:24:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -199,6 +199,8 @@ with nav_cols[2]:
     if st.button("🏷️ 완료된 공정 확인(선별)", key="nav_3", use_container_width=True): st.session_state.view = 'selection'; st.rerun()
 with nav_cols[3]:
     if st.button("🗃️ 모든 공정 이력 확인", key="nav_4", use_container_width=True): st.session_state.view = 'all_history'; st.rerun()
+with nav_cols[4]:
+    st.link_button("🌐일재고/재공현황", "https://myungin-pp.appsmith.com/app/untitled-application-1/page1-6a27d4bd9e8e4df7ae2343bf?environment=production", use_container_width=True)
 
 # --- 6. 사이드바 ---
 with st.sidebar:
